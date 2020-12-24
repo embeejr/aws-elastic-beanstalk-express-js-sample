18c630772ca84a3291a1c96d578d34e74c6fc330
Marty Boegner <boegner@stsci.edu>
2020-12-24 08:36:08 -0500

    personalized the web page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello Sucker, I Mean– World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
